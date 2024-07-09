ba5733917cb334ef01e16d2fabb881860f0f9f9e
artanton <kabletrade@gmail.com>
2024-07-10 01:25:16 +0300

    grid-stylyzation

--- src/components/taskItem/taskItemStyled.js
+++ src/components/taskItem/taskItemStyled.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 export const TaskRow = styled.div`
   display: flex;
-  width: '80 wv';
+  justify-content: space-between;
+  align-items: center;
+  padding: '10px 0';
+  width: 80vw,
   
-
-  gap: ${p => p.theme.gap.big};
 `;
 
 export const DeleteButton = styled.button`
   background: none;
   border: none;
- 
+
   cursor: pointer;
   display: flex;
   align-items: center;
